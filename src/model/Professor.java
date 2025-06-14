42a186e290a1d7565a2bc5e7421824c8c29fefe8
Erick E.C. Pereira <35979271+ztxy32@users.noreply.github.com>
2025-06-14 16:46:41 -0300

    update professor

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,5 +1,14 @@
 package model;
 
 public class Professor {
-
+	String cpf;
+	String nome;
+	String area;
+	float pontos;
+	
+	@Override
+	public String toString() {
+		return cpf+";"+nome+";"+area+";"+pontos+";";
+	}
+	
 }
